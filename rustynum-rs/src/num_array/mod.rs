23d0f55123a6fa86c449a0a56202eecc8fab33a0
IgorSusmelj <igor_7779@hotmail.com>
2025-05-31 18:22:07 +0200

    Move statistics into separate file

--- rustynum-rs/src/num_array/mod.rs
+++ rustynum-rs/src/num_array/mod.rs
@@ -4,5 +4,6 @@ mod impl_clone_from;
 pub mod linalg;
 mod manipulation;
 pub mod operations;
+mod statistics;
 
 pub use array_struct::{NumArray, NumArrayF32, NumArrayF64, NumArrayI32, NumArrayI64, NumArrayU8};
